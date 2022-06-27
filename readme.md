37aa8b9bac29484acfdb3aa291417c329170221a
Wahid Niam <iam.wahidn@gmail.com>
2022-06-27 13:57:42 +0700

    wip: trait bound with ExactSizeIterator

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 ## references
 - https://doc.rust-lang.org/std/iter/trait.Iterator.html
 - https://doc.rust-lang.org/stable/std/iter/
+- https://doc.rust-lang.org/stable/std/iter/trait.ExactSizeIterator.html
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,29 @@ const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 struct Progress<It> {
     iter: It,
     i: usize,
+    bound: Option<usize>,
 }
 
 // associate method with a Type
 // Impl block is quantified: for all type It implement progress of It, below call is valid to Progress
 impl<It> Progress<It> {
     pub fn new(iter: It) -> Self {
-        Progress { iter, i: 0 }
+        Progress {
+            iter,
+            i: 0,
+            bound: None,
+        }
+    }
+}
+
+//Bounded and Unbounded
+impl<It> Progress<It>
+where
+    It: ExactSizeIterator,
+{
+    pub fn with_bound(mut self) -> Self {
+        self.bound = Some(self.iter.len());
+        self
     }
 }
 
@@ -25,7 +41,15 @@ where
     // implement print progress whenever next() called, usually on loop
     fn next(&mut self) -> Option<Self::Item> {
         // print progress
-        println!("{}{}", CLEAR, "*".repeat(self.i));
+        print!("{}", CLEAR);
+        match self.bound {
+            Some(bound) => {
+                println!("[{}{}]", "*".repeat(self.i), " ".repeat(bound - self.i));
+            }
+            None => {
+                println!("{}", "*".repeat(self.i));
+            }
+        }
         self.i += 1;
         self.iter.next()
     }
@@ -43,9 +67,13 @@ impl<It> ProgressIteratorExt for It {
 }
 
 fn main() {
+    // unbouded
+    // for i in (0..).progress().with_bound() {
+    //     expensive_calculation(&i);
+    // }
     let v = vec![1, 2, 3, 4, 5];
     // Trait superpower start here, extending existing function without touching the function itself
-    for i in v.iter().progress() {
+    for i in v.iter().progress().with_bound() {
         expensive_calculation(i);
     }
 }
