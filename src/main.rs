fc8f6ba7df581c62c8969addc1c7347516e00ded
Wahid Niam <iam.wahidn@gmail.com>
2022-06-27 12:23:30 +0700

    wip: tqdm style

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,40 @@ use std::{thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-fn progress<It>(it: It, f: fn(It::Item) -> It::Item)
+struct Progress<It> {
+    iter: It,
+    i: usize,
+}
+
+// associate method with a Type
+// Impl block is quantified: for all type It implement progress of It, below call is valid to Progress
+impl<It> Progress<It> {
+    pub fn new(iter: It) -> Self {
+        Progress { iter, i: 0 }
+    }
+}
+
+// implement Iterator Trait
+// https://doc.rust-lang.org/std/iter/trait.Iterator.html
+impl<It> Iterator for Progress<It>
 where
-    It: Iterator, // constraint removed
+    It: Iterator,
 {
-    let mut s = 1; // state of progress
-    for i in it {
-        println!("{}{}", CLEAR, "*".repeat(s));
-        s += 1;
-        f(i);
+    type Item = It::Item;
+    // implement print progress whenever next() called, usually on loop
+    fn next(&mut self) -> Option<Self::Item> {
+        // print progress
+        println!("{}{}", CLEAR, "*".repeat(self.i));
+        self.i += 1;
+        self.iter.next()
     }
 }
 
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v.iter(), expensive_calculation);
+    for i in Progress::new(v.iter()) {
+        expensive_calculation(i);
+    }
 }
 
 fn expensive_calculation(_n: &i32) -> &i32 {
